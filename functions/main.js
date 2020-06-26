891828aa51f4cde090c024af51ede2a2d58ce572
bongiwentshangase <bongiwe.buselaphi@gmail.com>
2020-06-26 14:33:27 +0200

    funtions

---
+++ functions/main.js
@@ -0,0 +1,24 @@
+let name1="bongiwe";
+let name2="thandeka";
+let name3="nomcebo";
+
+
+//functions help to avoid repeating things.
+
+
+function greet(){
+    console.log(` Siyakwemukela oLundi.`);
+}
+
+function goodbye(){
+    console.log(`Usalekahle.ube nosuku oluhle `)
+}
+console.log(`Hello ${name1}`);
+greet();
+goodbye();
+console.log(`Hello ${name2}`);
+greet();
+goodbye();
+console.log(`Hello ${name3}`);
+greet();
+goodbye();
